3337c2911a5061058df8f71a02591af44357be74
BOURAOUI <mejri.bouraoui@gmail.com>
2018-10-30 17:06:40 +0100

    Scripted auto-commit on change (2018-10-30 17:06:39) by gitwatch.sh

--- python_libs/sqlite3.py
+++ python_libs/sqlite3.py
@@ -44,7 +44,7 @@ def update_by_id(conn,table,version,patch, id):
     cur = conn.cursor()
     sql = "update " + table + " set version=? , patch=? where id=? "
     cur.execute(sql, (version,patch,id,))
-    return return cur.fetchall()
+    return cur.fetchall()
 
 
 
