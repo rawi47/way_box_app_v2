c8b05ac19f5800d111c26dab2c254e1950f1b8be
BOURAOUI <mejri.bouraoui@gmail.com>
2018-10-30 16:26:19 +0100

    Scripted auto-commit on change (2018-10-30 16:26:19) by gitwatch.sh

--- python_libs/utils.py
+++ python_libs/utils.py
@@ -1,5 +1,7 @@
 
 import os
+import subprocess
+import datetime
 
 def _create_file(source,content,right):
     file = open(source,right)
@@ -9,3 +11,30 @@ def _create_file(source,content,right):
 def _create_dir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+def run(self,command,user,lst,printLog=True,getDate=True,shell=False):
+    sudo_password = user.password
+    #lst.append(command)
+    command = command.split()
+    try:
+        cmd1 = subprocess.Popen(['echo',sudo_password], stdout=subprocess.PIPE)
+        popen = subprocess.Popen(['sudo','-S'] + command, stdin=cmd1.stdout, stdout=subprocess.PIPE,shell=shell)
+        while True:
+            line = popen.stdout.readline()
+
+            if len(line) > 0:
+                getD = ""
+                if getDate:
+                    getD = str(datetime.datetime.now()) + " - "
+                lst.append(getD + line.decode().strip() )
+            else:
+                break
+
+        popen.wait()
+
+    except Exception as e:
+        lst.append ("OSError > " + str(e))
+    except:
+        lst.append ("Error > ")
+
+    return
