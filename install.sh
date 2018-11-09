86db3b7882497dfb0f7d44034fc21f2ed347323e
BOURAOUI <mejri.bouraoui@gmail.com>
2018-11-09 22:01:46 +0100

    changes in install.sh 0.1

--- install.sh
+++ install.sh
@@ -10,12 +10,13 @@ cd ..
 
 pip3 install -r requirements.txt
 
-cp -R opt/w.zone/ /var/www/
-cp -R opt/settings/nginx/sites-available/* /etc/nginx/sites-available
-cp -R opt/settings/systemd/system/middleware.service /etc/systemd/system/middleware.service
+sudo cp -rf opt/config/* /etc
+sudo cp -rf opt/w.zone/ /var/www/
+sudo cp -rf opt/settings/nginx/sites-available/* /etc/nginx/sites-available
+sudo cp -rf opt/settings/systemd/system/middleware.service /etc/systemd/system/middleware.service
 
-ln -s /etc/nginx/sites-available/middleware /etc/nginx/sites-enabled
-ln -s /etc/nginx/sites-available/w.club /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/middleware /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/w.club /etc/nginx/sites-enabled
 
-systemctl daemon-reload
-systemctl enable middleware
+sudo systemctl daemon-reload
+sudo systemctl enable middleware
