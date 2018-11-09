2776ebd0e5f19863c3dbcf0656c09ebd62a03391
BOURAOUI <mejri.bouraoui@gmail.com>
2018-11-09 21:59:54 +0100

    changes in install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,14 @@
-apt-get update
-apt-get -y upgrade
-sudo apt-get install -y hostapd ipset dnsmasq libmicrohttpd-dev python3 python3-dev python3-pip nginx-common nginx gunicorn  libpq-dev python-dev postgresql postgresql-contrib
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install -y hostapd ipset dnsmasq libmicrohttpd-dev python3 python3-dev python3-pip nginx-common nginx gunicorn  libpq-dev python-dev
 
 
 
-cd /opt/nodogsplash/
-make && make install
-cd ../
-cd ../
+cd opt/nodogsplash/
+sudo make && make install
+cd ..
 
-cd opt/
 pip3 install -r requirements.txt
-cd ../
-
 
 cp -R opt/w.zone/ /var/www/
 cp -R opt/settings/nginx/sites-available/* /etc/nginx/sites-available
@@ -21,9 +17,5 @@ cp -R opt/settings/systemd/system/middleware.service /etc/systemd/system/middlew
 ln -s /etc/nginx/sites-available/middleware /etc/nginx/sites-enabled
 ln -s /etc/nginx/sites-available/w.club /etc/nginx/sites-enabled
 
-systemctl enable middleware
 systemctl daemon-reload
-
-echo "0 5 * * * python3 /home/pi/way_box_app_v2/update.py" >> cron
-crontab cron
-rm cron
+systemctl enable middleware
