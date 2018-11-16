17dc4087c6b0952fda55a5529ff4af866af177d0
BOURAOUI <mejri.bouraoui@gmail.com>
2018-11-16 10:37:24 +0100

    update system 3

--- middleware/boxes/admin.py
+++ middleware/boxes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import BoxStatus, Patches, Script, UpdateInformation
+from .models import BoxStatus, Patches, Script
 
 admin.site.register(BoxStatus)
 admin.site.register(Patches)
