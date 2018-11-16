94a7fc9cc40e8e0b36b9570c00a7b2547889b610
BOURAOUI <mejri.bouraoui@gmail.com>
2018-11-16 10:36:34 +0100

    update system 3

--- middleware/boxes/admin.py
+++ middleware/boxes/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from safedelete.admin import highlight_deleted, SafeDeleteAdmin
 from .models import BoxStatus, Patches, Script, UpdateInformation
 
 admin.site.register(BoxStatus)
