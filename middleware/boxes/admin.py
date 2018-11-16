a8f03ecf4cde6843fee26fbc35adc2314f2cb391
BOURAOUI <mejri.bouraoui@gmail.com>
2018-11-16 10:34:45 +0100

    update system 3

--- middleware/boxes/admin.py
+++ middleware/boxes/admin.py
@@ -3,23 +3,4 @@ from safedelete.admin import highlight_deleted, SafeDeleteAdmin
 from .models import BoxStatus, Patches, Script, UpdateInformation
 
 admin.site.register(BoxStatus)
-
-
-class ScriptInline(admin.TabularInline):
-    model = Script
-    ordering = ('sequence',)
-
-
-@admin.register(Patches)
-class PatchesAdmin(SafeDeleteAdmin):
-    list_display = (
-        highlight_deleted, 'created_at',
-        *SafeDeleteAdmin.list_display
-    )
-
-    search_fields = (
-        'name',
-        *SafeDeleteAdmin.list_filter
-    )
-
-    inlines = (ScriptInline,)
+admin.site.register(Patches)
