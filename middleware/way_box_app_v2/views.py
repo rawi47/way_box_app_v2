edc0c8f736099f317d3d0537a1d4b8a0e7abc306
BOURAOUI <mejri.bouraoui@gmail.com>
2018-11-16 18:51:51 +0100

    Fix absolut path for manage

--- middleware/way_box_app_v2/views.py
+++ middleware/way_box_app_v2/views.py
@@ -44,11 +44,10 @@ def catch_all(request,path):
             params[key] = value
         signature = sign(API_KEY, API_SECRET, params)
     elif request.method == 'POST':
-        data = {}
         try:
             data = json.loads(request.body.decode('utf-8'))
-        except Exception as e:
-            log.error(str(e))
+        except Exception:
+            data = {}
         signature = sign(API_KEY, API_SECRET, data)
     else:
         dataOpt = {}
