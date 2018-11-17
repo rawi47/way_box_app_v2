024888fd9c87b4184d4e26bc120e4c04b60948e7
BOURAOUI <mejri.bouraoui@gmail.com>
2018-11-17 11:44:56 +0100

    Adding exception in make_request

--- middleware/utils/httpHandler.py
+++ middleware/utils/httpHandler.py
@@ -65,6 +65,6 @@ class Httphandler(models.Model):
 		if method == "GET":
 		    res = self._requests_retry_session().get(url)
 		elif method == "POST":
-			data = json.dumps(data)
+			# data = json.dumps(data)
 			res = self._requests_retry_session().post(url,data=data)
 		return res
