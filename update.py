6f1d06843162d2cdd2bb5615f3ac6d85f5d3f5ca
BOURAOUI <mejri.bouraoui@gmail.com>
2018-11-19 16:56:59 +0100

    stop reboot after update

--- update.py
+++ update.py
@@ -58,7 +58,7 @@ if git_hash_remote != git_hash_local:
     print("inside")
     utils.run(cmd_pull,password,lst)
     utils.run(cmd_migrate,password,lst)
-    utils.run(cmd_reboot,password,lst)
+    # utils.run(cmd_reboot,password,lst)
 
 
 
